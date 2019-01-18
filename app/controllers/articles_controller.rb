d20344ed86194c0b8b27127de024bb7a97da3fcd
Leonardo Kelava <leonardo.kelava2@gmail.comgit config --global color.ui autogit config -- get user.namegit config -- get user.namegit config --global user.name Leonardo>
2019-01-18 19:51:52 +0100

    Second commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,45 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
     def index
         @articles= Article.all
     end
 
     def show
         @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+
+        flash.notice = " Article '#{@article.title}' created"
+       
+     @article.save
+        redirect_to article_path(@article)
+    end
+
+    def destroy
+        Article.find(params[:id]).destroy
+        
+        redirect_to articles_path
+
+        
+    end
+
+    def edit
+        @article=Article.find(params[:id])
+    end
+
+    def update
+      @article = Article.find(params[:id])
+      @article.update(article_params)
+
+      flash.notice = "Article '#{@article.title}' updated"
+
+      redirect_to article_path(@article)
+
     end
 end
